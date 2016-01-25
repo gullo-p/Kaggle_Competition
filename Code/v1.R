93f196729cbdb1414dc7006427c14addfa3dec46
Guglielmo <guglielmo.pelino@barcelonagse.eu>
2016-01-26 00:13:51 +0100

    latest version

--- Code/v1.R
+++ Code/v1.R
@@ -1,6 +1,6 @@
 library(class)
 train <- read.csv("../DATA/news_popularity_training.csv", sep = ",")
-
+test <- read.csv("../DATA/news_popularity_test.csv", sep = ",")
 #tables
 table(train$popularity)
 round(prop.table(table(train$popularity)) * 100, digits = 1)
@@ -19,16 +19,31 @@ train_norm <- cbind(train[1:3],train_norm,train$popularity)
 ?sample
 #Sample
 ind <- sample(2, nrow(train), replace=TRUE, prob=c(0.67, 0.33))
-train.training <- train[ind==1, 1:61]
-train.test <- train[ind==2, 1:61]
+train.training <- train[ind==1, 3:61]
+train.test <- train[ind==2, 3:61]
 train.trainLabels <- train[ind==1, 62]
 train.testLabels <- train[ind==2, 62]
 
 
-
+c(1:5)
 
 #kkn
-train_pred <- knn(train = train.training, test = train.test, cl = train.trainLabels, k=3)
+train_pred <- knn(train = train.training, test = train.test, cl = train.trainLabels, k=20)
+train_pred <- as.data.frame(train_pred)
 
-?knn
-iris3[,,2]
+#compare the results with the truth
+result <- cbind(train_pred, train.testLabels)
+
+#rate of correctness
+sum(result$train_pred == train.testLabels)/9912
+
+#real test to submit
+real_train_pred <- knn(train=train[,3:61], test = test[,3:61], cl = train[,62], k=20)
+real_train_pred <- as.data.frame(real_train_pred)
+
+#data frame to submit
+submit <- cbind(c(30001:39644), real_train_pred)
+names(submit) <- c("id", "popularity")
+
+write.csv(submit, file = "submit1.csv", quote = FALSE, row.names = FALSE)
+
