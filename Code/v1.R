f6f9d54478196e4320c160386b8d4fd4e4b7381c
Corretx <anna.corretger@barcelonagse.eu>
2016-01-26 12:41:58 +0100

    code with variables year and month

--- Code/v1.R
+++ Code/v1.R
@@ -1,12 +1,67 @@
 library(class)
 train <- read.csv("../DATA/news_popularity_training.csv", sep = ",")
 test <- read.csv("../DATA/news_popularity_test.csv", sep = ",")
+
 #tables
 table(train$popularity)
 round(prop.table(table(train$popularity)) * 100, digits = 1)
 
 
-#normalize
+
+#STANDARDIZED VERSION  +  YEAR AND MONTH (THE ONE WHO WORKED BEST)
+standardize <- function(x) {
+  num <- x - mean(x)
+  denom <-sd(x)
+  return (num/denom)
+}
+
+train_stand <- as.data.frame(lapply(train[4:61], standardize))
+
+#obtain year and month from url
+train$year <- sapply(train$url, FUN=function(x) {as.numeric(substring(x, 21,24))})
+train$month <- sapply(train$url, FUN=function(x) {as.numeric(substring(x, 26,27))})
+
+train_stand <- cbind(train$id, train$url, train$timedelta,train_stand,train$year, train$month, train$popularity)
+
+
+ind <- sample(2, nrow(train_stand), replace=TRUE, prob=c(0.67, 0.33))
+train.training <- train_stand[ind==1, 3:63]
+train.test <- train_stand[ind==2, 3:63]
+train.trainLabels <- train_stand[ind==1, 64]
+train.testLabels <- train_stand[ind==2, 64]
+
+#kkn
+train_pred <- knn(train = train.training, test = train.test, cl = train.trainLabels, k=20)
+train_pred <- as.data.frame(train_pred)
+
+#compare the results with the truth
+result <- cbind(train_pred, train.testLabels)
+
+#rate of correctness
+sum(result$train_pred == train.testLabels)/10008
+
+test_stand <- as.data.frame(lapply(test[4:61], standardize))
+
+#obtain year and month from url
+test$year <- sapply(test$url, FUN=function(x) {as.numeric(substring(x, 21,24))})
+test$month <- sapply(test$url, FUN=function(x) {as.numeric(substring(x, 26,27))})
+
+test_stand <- cbind(test[1:3],test_stand , test$year , test$month)
+
+#real test to submit
+real_train_pred <- knn(train=train_stand[,3:63], test = test_stand[,3:63], cl = train_stand[,64], k=20)
+real_train_pred <- as.data.frame(real_train_pred)
+
+#data frame to submit
+submit <- cbind(c(30001:39644), real_train_pred)
+names(submit) <- c("id", "popularity")
+
+write.csv(submit, file = "submit3.csv", quote = FALSE, row.names = FALSE)
+
+
+----------------------------------------------------
+
+#NORMALIZED VERSION 
 normalize <- function(x) {
   num <- x - min(x)
   denom <- max(x) - min(x)
@@ -16,14 +71,13 @@ train_norm <- as.data.frame(lapply(train[4:61], normalize))
 train_norm <- cbind(train[1:3],train_norm,train$popularity)
 
 
-?sample
+
 #Sample
 ind <- sample(2, nrow(train), replace=TRUE, prob=c(0.67, 0.33))
 train.training <- train[ind==1, 3:61]
 train.test <- train[ind==2, 3:61]
 train.trainLabels <- train[ind==1, 62]
 train.testLabels <- train[ind==2, 62]
-
 
 
 #kkn
@@ -64,8 +118,9 @@ result <- cbind(train_pred, train.testLabels)
 #rate of correctness
 sum(result$train_pred == train.testLabels)/9952
 
+-----------------------------------------------------------------
 
-#STANDARDIZED VERSION (THE ONE WHO WORKED BEST)
+#STANDARDIZED VERSION 
 standardize <- function(x) {
   num <- x - mean(x)
   denom <-sd(x)
@@ -103,5 +158,3 @@ submit <- cbind(c(30001:39644), real_train_pred)
 names(submit) <- c("id", "popularity")
 
 write.csv(submit, file = "submit2.csv", quote = FALSE, row.names = FALSE)
-
-
