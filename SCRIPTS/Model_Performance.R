25f5f415b43883a9453502d85bb795670bb75a71
Santhosh Narayanan <santhoshnarayanan@Santhoshs-MacBook-Pro.local>
2016-03-17 23:14:10 +0100

    Report

--- SCRIPTS/Model_Performance.R
+++ SCRIPTS/Model_Performance.R
@@ -39,21 +39,11 @@ time.data$missing.flag[time.data$global_subjectivity == 0] <- 1
 # add the date variables (year, month and is.holiday)
 time.data <- obtain.date(time.data)
 
-# add variable day
-time.data$day <- as.factor(substring(time.data$url, 29,30))
-
 # add the quarter variable
-time.data$quarter <- ifelse(time.data$month < 4, 1,
-                            ifelse(time.data$month < 7, 2,
-                                   ifelse(time.data$month < 10, 3,4)))
-
-# add how many articles in the same date
-time.data$countsDay <- ave(time.data$timedelta, time.data$timedelta, FUN = length)
-
+time.data$quarter <- cut(time.data$ts,breaks=8,labels=1:8)
 
 # Convert the response to ordinal
-time.data$popularity <- factor(time.data$popularity)
-
+time.data$popularity <- factor(time.data$popularity, ordered = TRUE)
 
 # Add channel other
 time.data$data_channel_is_other <- ifelse(time.data$data_channel_is_lifestyle == 0 &
@@ -62,9 +52,16 @@ time.data$data_channel_is_other <- ifelse(time.data$data_channel_is_lifestyle ==
                                             time.data$data_channel_is_tech == 0 &
                                             time.data$data_channel_is_world == 0, 1,0)
 
+# Add variable day
+time.data$day <- as.factor(substring(time.data$url, 29,30))
 
-#0.5350
+# Add how many post the same date
+time.data$countsDay <- ave(time.data$timedelta, time.data$timedelta, FUN = length)
 
+#Change quarter
+time.data$quarter <- ifelse(time.data$month < 4, 1,
+                            ifelse(time.data$month < 7, 2,
+                                   ifelse(time.data$month < 10, 3,4)))
 
 # New variables according to words in the url
 techBrands <- c("facebook|roomba|acer|google|3g|wikipedia|app|apple|ibm|ios|iphone|blackberry|xbox|playstation|ipad|windows|dell|pc|samsung|microsoft|lenovo|yahoo|asus|amazon|lg|gopro|hp|sony|bill-gates|htc|nintendo|nokia|beatsvbing|bitcoin|imac|kindle|drone")
@@ -77,7 +74,7 @@ socialMedia <- c("facebook|evernote|myspace|twitter|instagram|snapchat|uber|vine
 
 sports <- c("cup|ping-pong|crossfit|olympics|scoccer|nhl|clippers|football|rugby|fifa|suarez|skating|michael-jordan|ferguson|chelsea|nfl|messi|guardiola|baseball|basket|nba|nfl|golf|tiger-woods|lebron-james")
 
-cars <- c("toyota|audi|car")
+cars <- c("toyota|audi|super-bowl|car|aston-martin|mercedes|ford|nissan")
 
 tvShows <- c("star-trek|sitcom|hbo|armstrong|tv-premier|doctor-who|how-i-met-your-mother|spoiler|tv-show|castle|season|thrones|true-blood|star-wars|potter|sons-of-anarchy|superman|sherlock|walter-white")
 
@@ -98,24 +95,33 @@ time.data$isPopular <- ifelse(time.data$techBrands == 0 &
                                 time.data$cars == 0 &
                                 time.data$tvShows == 0, 0,1)
 
-# PREDICTIONS
-##########################################################################################
-# Train and predict using random forest on Rolling windows
-rf.predictions <- rolling.windows(dataset = time.data, step.size = 1000,
-                                  FUN = my.forest, ntree = 300, mtry = 4 )
 
-rf.predictions <- rf.predictions[,c(1,32)]
-colnames(rf.predictions) <- c("id","popularity")
+data = time.data[time.data$flag == 0,]
+n = 10
+m = 22000
 
-# call the text mining function for getting some more predictions
-pred <- predictLabels(time.data)
+for(iter in 1:n){
+  
+  index = sample(1:nrow(data),m)
+  data[index,'flag'] <- 0
+  data[-index,'flag'] <- 1
+  data = data[order(data$ts),]
+  
+  rf.predictions <- rolling.windows(dataset = data, step.size = 1000,
+                                    FUN = my.forest, ntree = 300, mtry = 4 )
+  
+  rf.predictions <- rf.predictions[,c(1,32)]
+  colnames(rf.predictions) <- c("id","popularity")
+  
+  # call the text mining function for getting some more predictions
+  pred <- predictLabels(time.data)
+  
+  # merge the two predictions 
+  final.prediction <- merge(pred, rf.predictions, by.x = "id", by.y = "id")
+  final.prediction$popularity <- ifelse(is.na(final.prediction$popularity.x), 
+                                        final.prediction$popularity.y, final.prediction$popularity.x)
+  
+  final.prediction$popularity.x <- NULL
+  final.prediction$popularity.y <- NULL
+}
 
-# merge the two predictions 
-final.prediction <- merge(pred, rf.predictions, by.x = "id", by.y = "id")
-final.prediction$popularity <- ifelse(is.na(final.prediction$popularity.x), 
-                                      final.prediction$popularity.y, final.prediction$popularity.x)
-
-final.prediction$popularity.x <- NULL
-final.prediction$popularity.y <- NULL
-
-write.table(final.prediction,"submit.csv",sep=",",row.names = F)
